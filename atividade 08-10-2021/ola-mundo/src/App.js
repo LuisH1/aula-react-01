f74435f8153028c4090000589f3bbd0a534904e7
Luis Henrique <LuisHenSilva@Outlook.com>
2021-10-13 22:05:27 -0300

    aula react 1

---
+++ atividade 08-10-2021/ola-mundo/src/App.js
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+
+function App() {
+  return (
+    <div>
+      <h1>Aprendendo react</h1>
+
+      <img src="https://www.iguiecologia.com/wp-content/uploads/2021/02/Coruja-buraqueira7-2.jpg" alt=""/>
+      <pre > Por influência da mitologia grega, tanto que Atena, deusa da guerra e da sabedoria, 
+        tinha uma coruja como mascote, Os gregos consideravam a noite como o momento do pensamento filosófico e da revelação</pre>
+    </div>
+  );
+  
+}
+
+export default App;
